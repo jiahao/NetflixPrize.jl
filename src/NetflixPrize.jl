57b6f4b46299a597380ec118b64af233b6e47056
Jiahao Chen <jiahao@mit.edu>
2016-05-14 20:47:25 -0400

    Fully automate data parsing and loading
    load() can now from the tarball in the user's Downloads directory
    and goes through the stages of unpacking the tarball, parsing all the
    text files in the training set, and storing the resulting data as a
    sparse matrix.

--- src/NetflixPrize.jl
+++ src/NetflixPrize.jl
@@ -1,4 +1,5 @@
 #Netflix Prize data set
+module NetflixPrize
 
 using Glob
 using JLD
@@ -11,17 +12,33 @@ function parsefile(filename)
     movieid, userratings
 end
 
-function parseall(dir, outputfile="../data/training_set.jld")
-    info("Parsing training set")
+function parseall(datadir, outputfile=joinpath(Pkg.dir("NetflixPrize"), "data", "training_set.jld"))
+    files = cd(()->glob("mv_*.txt"), datadir)
 
-    maxusers, allmovies, allnrows, allusers, allratings =
-    @time pmap(glob(joinpath(dir, "mv_*.txt"))) do file
-        movieid, userratings = parsefile(file)
+    info("Parsing training set from $(length(files)) files")
+
+    @time parsed_data = pmap(files) do file
+        movieid, userratings = parsefile(joinpath(datadir, file))
         users =  map(Int, sub(userratings,:,1))
         ratings = map(Int, sub(userratings,:,2))
         nrows = size(userratings, 1)
         maximum(users), movieid, nrows, users, ratings
     end
+
+    maxusers = [item[1] for item in parsed_data]
+    allmovies = [item[2] for item in parsed_data]
+    allnrows = [item[3] for item in parsed_data]
+    allusers = [item[4] for item in parsed_data]
+    allratings = [item[5] for item in parsed_data]
+
+    # info("Saving intermediate data set to $outputfile")
+    # JLD.save(outputfile,
+    #     "maxusers", maxusers,
+    #     "allmovies", allmovies,
+    #     "allnrows", allnrows,
+    #     "allusers", allusers,
+    #     "allratings", allratings
+    # )
 
     maxuser=maximum(maxusers)
     maxmovieid=maximum(allmovies)
@@ -41,18 +58,43 @@ function parseall(dir, outputfile="../data/training_set.jld")
         end
         Js[globalidx+1:globalidx+nentries] = allusers[chunkid]
         Vs[globalidx+1:globalidx+nentries] = allratings[chunkid]
+        globalidx += nentries
     end
 
-    M = sparse(Is, Js, Vs)
+    @time M = sparse(Is, Js, Vs)
 
     info("Saving training set to $outputfile")
     JLD.save(outputfile, "data", M)
 end
 
-addprocs(CPU_CORES-nprocs()+1)
-datadir = "../data/download/training_set"
-if isdir(datadir)
-    parseall(datadir)
-else
-    error("Place Netflix Prize training set in $datadir/mv_*.txt")
+function load()
+    const datadir = joinpath(Pkg.dir("NetflixPrize"), "data/download/training_set")
+    const datafile = joinpath(Pkg.dir("NetflixPrize"), "data/training_set.jld")
+    const tarball = joinpath(Pkg.dir("NetflixPrize"), "data/nf_prize_dataset.tar.gz")
+    if !isdir(datadir)
+        #If tarball found in user's download directory, move it into the package subtree
+        const dltarball = joinpath(ENV["HOME"], "Downloads", "nf_prize_dataset.tar.gz")
+        if isfile(dltarball)
+            info("Copying training set tarball to $tarball")
+            isdir(dirname(tarball)) || mkdir(dirname(tarball))
+            cp(dltarball, tarball)
+        end
+
+        if isfile(tarball)
+            info("Unpacking training set from $tarball")
+            run(Cmd(`tar xzf $tarball`, dir=dirname(tarball)))
+            run(Cmd(`tar xf training_set.tar`, dir=joinpath(dirname(tarball), "download")))
+        else
+            error("Place Netflix Prize training set in $tarball")
+        end
+    end
+
+    if !isfile(datafile)
+        info("Parsing Netflix prize data from $datadir into $datafile")
+        parseall(datadir)
+    end
+
+    data = load(datafile, "data")
+end
+
 end
